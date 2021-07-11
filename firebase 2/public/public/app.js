d1e1a3b8b049143bce793c5e5f2cdd9f6f186a03
Zaaks-Projects <82881610+Zaaks-Projects@users.noreply.github.com>
2021-07-11 21:16:55 +0200

    Updates

---
+++ firebase 2/public/public/app.js
@@ -0,0 +1,77 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCPWcauJpBREVKVRn3BmQbNaDaCp0J_eao",
+    authDomain: "portfolio-contactform-fb210.firebaseapp.com",
+    projectId: "portfolio-contactform-fb210",
+    storageBucket: "portfolio-contactform-fb210.appspot.com",
+    messagingSenderId: "998740486486",
+    appId: "1:998740486486:web:d68cba2791851afc338ef7"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+//   Referencing my collection
+  let Information = firebase.database().ref("This is the latest mails");
+
+
+
+  document.querySelector(".contact-form").addEventListener("submit", submitForm);
+
+  function submitForm(e) {
+      e.preventDefault();
+      
+      let name = document.querySelector(".name").value;
+      let email = document.querySelector(".email").value;
+      let reason = document.querySelector(".reason").value;
+      let message = document.querySelector(".message").value;
+      console.log(name, email, reason, message);
+
+      saveInfomation(name, email, reason, message);
+      
+  }
+
+  //Saving the information 
+  function saveInfomation(name, email, reason, message){
+      let Info = Information.push();
+
+      Info.set({
+          fullname: name,
+          email: email,
+          selectthereason: reason,
+          message: message,
+
+      });
+
+      window.alert("Hi, I recieved your mail")
+  }
+
+
+// Email: to the personal email accout
+let a = {
+        
+}
+function updateMessage(key, value){
+
+    a[key] = value;
+
+}
+function sendEmail(){
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username:"zaakiraha34@gmail.com",
+        Password:"ytmtnsdhtzetiaro",
+        To: 'zaakiraha34@gmail.com',
+        From: `Email: ${a["data[email]"]}`,
+        Subject: "New Email",
+        Body: `Name: ${a["data[name]"]} <br> ${a["data[message]"]}`
+    });
+}
+
+firebase.initializeApp({
+    // Your firebase configuration object
+  });
+  
+  const appCheck = firebase.appCheck();
+  // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+  // key is the counterpart to the secret key you set in the Firebase console.
+  appCheck.activate('6Lf5gYwbAAAAAKwDCOCfB-MX1-5PiS9dyuNKmef6');
